f302ad3ceb42232675a072bcca8454590e3f0328
shantanu69 <rahmanshantanu69@gmail.com>
2016-02-07 13:38:00 +0600

    Fix serious bugs in queue & stack

--- src/lcthw/queue.h
+++ src/lcthw/queue.h
@@ -1,12 +1,14 @@
-#ifndef _lcthw_stack_h
-#define _lcthw_stack_h
+#ifndef _lcthw_queue_h
+#define _lcthw_queue_h
 
 #include <lcthw/list.h>
 
-#define Queue List
+typedef List Queue;
 
 #define Queue_create() (List_create())
 #define Queue_destroy(A) (List_destroy(A))
+#define Queue_clear(A) (List_clear(A))
+#define Queue_clear_destroy(A) (List_clear_destroy(A))
 
 #define Queue_peek(A) (List_first(A))
 #define Queue_count(A) (List_count(A))
@@ -14,7 +16,7 @@
 #define Queue_send(A, B) (List_push(A, B))
 #define Queue_recv(A) (List_shift(A))
 
-#define QUEUE_FOREACH(A, B) LIST_FOREACH(A, first, next, B)
+#define QUEUE_FOREACH(A, B) LIST_FOREACH(A, last, prev, B)
 
 #endif
 
--- src/lcthw/stack.h
+++ src/lcthw/stack.h
@@ -3,10 +3,12 @@
 
 #include <lcthw/list.h>
 
-#define Stack List
+typedef List Stack;
 
 #define Stack_create() (List_create())
 #define Stack_destroy(A) (List_destroy(A))
+#define Stack_clear(A) (List_clear(A))
+#define Stack_clear_destroy(A) (List_clear_destroy(A))
 
 #define Stack_peek(A) (List_first(A))
 #define Stack_count(A) (List_count(A))
@@ -14,7 +16,7 @@
 #define Stack_push(A, B) (List_unshift(A, B))
 #define Stack_pop(A) (List_shift(A))
 
-#define STACK_FOREACH(A, B) LIST_FOREACH(A, first, next, B)
+#define STACK_FOREACH(A, B) LIST_FOREACH(A, last, prev, B)
 
 #endif
 
